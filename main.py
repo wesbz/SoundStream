bfd0a46b915c28b747928eb567ec5859cec549bb
Alexander Trimm <alex@Alexanders-MacBook-Pro.local>
2022-04-12 13:44:34 -0400

    Factor out a full train/evaluation cycle into a shared function

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from torchaudio.transforms import MelSpectrogram
 from tqdm import tqdm
 
 from net import SoundStream, WaveDiscriminator, STFTDiscriminator
-from loss import adversarial_g_loss, feature_loss, adversarial_d_loss, spectral_reconstruction_loss
+from loss import criterion_d, criterion_g
 from dataset import NSynthDataset
+from ops import evaluate
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -28,7 +29,6 @@ soundstream.to(device)
 wave_disc.to(device)
 stft_disc.to(device)
 
-
 def collate_fn(batch):
     lengths = torch.tensor([elem.shape[-1] for elem in batch])
     return nn.utils.rnn.pad_sequence(batch, batch_first=True), lengths
@@ -47,9 +47,6 @@ test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, collate_fn=collate
 optimizer_g = optim.Adam(soundstream.parameters(), lr=1e-4, betas=(0.5, 0.9))
 optimizer_d = optim.Adam(list(wave_disc.parameters()) + list(stft_disc.parameters()), lr=1e-4, betas=(0.5, 0.9))
 
-criterion_g = lambda x, G_x, features_stft_disc_x, features_wave_disc_x, features_stft_disc_G_x, features_wave_disc_G_x, lengths_wave, lengths_stft, sr, dev: LAMBDA_ADV*adversarial_g_loss(features_stft_disc_G_x, features_wave_disc_G_x, lengths_stft, lengths_wave) + LAMBDA_FEAT*feature_loss(features_stft_disc_x, features_wave_disc_x, features_stft_disc_G_x, features_wave_disc_G_x, lengths_wave, lengths_stft) + LAMBDA_REC*spectral_reconstruction_loss(x, G_x, sr, device)
-criterion_d = adversarial_d_loss
-
 best_model = soundstream.state_dict().copy()
 best_val_loss = float("inf")
 
@@ -59,57 +56,14 @@ history = {
     "test": {"d": [], "g": []}
 }
 
+transform = lambda x: torch.stft(x.squeeze(), n_fft=1024, hop_length=256, window=torch.hann_window(window_length=1024, device=device), return_complex=False).permute(0, 3, 1, 2)
+
+
 for epoch in range(1, N_EPOCHS+1):
-    
-    soundstream.train()
-    stft_disc.train()
-    wave_disc.train()
-    
-    train_loss_d = 0.0
-    train_loss_g = 0.0
-    for x, lengths_x in tqdm(train_loader):
-        x = x.to(device)
-        lengths_x = lengths_x.to(device)
-        
-        G_x = soundstream(x)
-        
-        s_x = torch.stft(x.squeeze(), n_fft=1024, hop_length=256, window=torch.hann_window(window_length=1024, device=device), return_complex=False).permute(0, 3, 1, 2)
-        lengths_s_x = 1 + torch.div(lengths_x, 256, rounding_mode="floor")
-        s_G_x = torch.stft(G_x.squeeze(), n_fft=1024, hop_length=256, window=torch.hann_window(window_length=1024, device=device), return_complex=False).permute(0, 3, 1, 2)
-        
-        lengths_stft = stft_disc.features_lengths(lengths_s_x)
-        lengths_wave = wave_disc.features_lengths(lengths_x)
-        
-        features_stft_disc_x = stft_disc(s_x)
-        features_wave_disc_x = wave_disc(x)
-        
-        features_stft_disc_G_x = stft_disc(s_G_x)
-        features_wave_disc_G_x = wave_disc(G_x)
-        
-        loss_g = criterion_g(x, G_x, features_stft_disc_x, features_wave_disc_x, features_stft_disc_G_x, features_wave_disc_G_x, lengths_wave, lengths_stft)
-        train_loss_g += loss_g.item()
-        
-        optimizer_g.zero_grad()
-        loss_g.backward()
-        optimizer_g.step()
-        
-        features_stft_disc_x = stft_disc(s_x)
-        features_wave_disc_x = wave_disc(x)
-        
-        features_stft_disc_G_x_det = stft_disc(s_G_x.detach())
-        features_wave_disc_G_x_det = wave_disc(G_x.detach())
-        
-        loss_d = criterion_d(features_stft_disc_x, features_wave_disc_x, features_stft_disc_G_x_det, features_wave_disc_G_x_det, lengths_stft, lengths_wave)
-        
-        train_loss_d += loss_d.item()
-        
-        optimizer_d.zero_grad()
-        loss_d.backward()
-        optimizer_d.step()
-    
-    history["train"]["d"].append(train_loss_d/len(train_loader))
-    history["train"]["g"].append(train_loss_g/len(train_loader))
-    
+
+    evaluate(soundstream, stft_disc, wave_disc, transform. train_loader, optimizer_g, optimizer_d, device, sr, train=True, history=hisory['train'])
+    print(history['train'])
+
     with torch.no_grad():
         stft_disc.eval()
         wave_disc.eval()
@@ -121,11 +75,10 @@ for epoch in range(1, N_EPOCHS+1):
             lengths_x = lengths_x.to(device)
         
             G_x = soundstream(x)
-            
             s_x = torch.stft(x.squeeze(), n_fft=1024, hop_length=256, window=torch.hann_window(window_length=1024, device=device), return_complex=False).permute(0, 3, 1, 2)
             lengths_s_x = 1 + torch.div(lengths_x, 256, rounding_mode="floor")
             s_G_x = torch.stft(G_x.squeeze(), n_fft=1024, hop_length=256, window=torch.hann_window(window_length=1024, device=device), return_complex=False).permute(0, 3, 1, 2)
-            
+
             lengths_stft = stft_disc.features_lengths(lengths_s_x)
             lengths_wave = wave_disc.features_lengths(lengths_x)
             
